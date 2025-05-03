d37a9a0816b52778635cbce0040ecf7380c84a91
kgaogelot <kgaogelot@tradeshield.ai>
2025-05-03 17:41:14 +0200

    Nav bar

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
+import { NgIf } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [FormsModule, NgIf, BsDropdownModule],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+export class NavComponent {
+  private accountService = inject(AccountService);
+  loggedIn = false;
+  model: any = {};
+
+  login(){
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      error: error => console.log(error)
+      }
+    )
+    console.log(this.model);
+  }
+
+  logout(){
+    this.loggedIn = true;
+  }
+
+}
