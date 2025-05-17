ab57a51c4074e96dc12030c814b97d956001daa2
kgaogelot <kgaogelot@tradeshield.ai>
2025-05-17 23:30:57 +0200

    Route guards and toaster

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, inject, input, output,  signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +11,8 @@ import { AccountService } from '../_services/account.service';
   styleUrls: ['./register.component.css'] // corrected key
 })
 export class RegisterComponent {
-  private accountService = inject(AccountService)
+  private accountService = inject(AccountService);
+  private toastr = inject(ToastrService);
   cancelRegister = output<boolean>();
   model: any = {}
 
@@ -20,7 +22,7 @@ export class RegisterComponent {
         console.log(response);
         this.cancel();
       },
-      error: error => console.log(error)
+      error: error => this.toastr.error(error.error)
     })
   }
 
